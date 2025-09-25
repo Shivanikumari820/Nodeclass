447c6d08ba4536fda8f458f8cc899fb11f7005cd
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-09-25 15:19:31 +0530

    done

---
+++ LogFormUsingCookies/backend1/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173", // React frontend
+  credentials: true
+}));
+
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/cookieAuth", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("Database connected"))
+  .catch(err => console.error("DB connection error:", err));
+
+// Routes
+app.use("/", authRoutes);
+
+app.listen(5000, () => console.log("Server running on http://localhost:5000"));

50961670621594312a7b602a49c89c5a331fe88b
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-09-30 23:27:14 +0530

    done

--- socket_io/backend1/index.js
+++ socket_io/backend1/index.js
@@ -1,37 +1,39 @@
-const express = require('express');
-const https = require('https');
-const {Server} = require("socket.io");
-const cors = require('cors');
+// const express = require('express');
+// const https = require('https');
+// const {Server} = require("socket.io");
+// const cors = require('cors');
 
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
 
-const httpsServer = https.createServer(app);
+// const httpsServer = https.createServer(app);
 
-//Setup Socket.io server
-const io = new Server(httpsServer, {
-    cors: {
-        origin: "http://localhost:5173",         // React app origin
-        methods: ["GET", "POST"]
-    }
-});
+// //Setup Socket.io server
+// const io = new Server(httpsServer, {
+//     cors: {
+//         origin: "http://localhost:5173",         // React app origin
+//         methods: ["GET", "POST"]
+//     }
+// });
 
-io.on("connection", (socket) => {
-    console.log(`User connected: ${socket.id}`);
+// io.on("connection", (socket) => {
+//     console.log(`User connected: ${socket.id}`);
 
-    socket.on("send_message", (data) => {
-      console.log(data);
-      //Broadcast message to all clients except sender
-        socket.broadcast.emit("receive_message", data);
-    });
+//     socket.on("send_message", (data) => {
+//       console.log(data);
+//       //Broadcast message to all clients except sender
+//         socket.broadcast.emit("receive_message", data);
+//     });
 
-    socket.on("disconnect", () => {
-        console.log("User disconnected", socket.id);
-    });
-});
+//     socket.on("disconnect", () => {
+//         console.log("User disconnected", socket.id);
+//     });
+// });
 
-httpsServer.listen(5000, () => {
-    console.log("Server is running on https://localhost:5000");
-});
+// httpsServer.listen(5000, () => {
+//     console.log("Server is running on https://localhost:5000");
+// });
+
+
 
